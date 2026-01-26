34a5d173cc827f25fb2be12c77a41a7e0027d018
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-01-26 11:22:26 -0500

    feat: removing all source code in preperation for documentation

--- src/config/file_ops.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-
-pub fn read_file(path: &str) -> Result<String, std::io::Error> {
-    fs::read_to_string(path)
-}
-
-pub fn write_file(path: &str, content: &str) -> Result<(), std::io::Error> {
-    fs::write(path, content)
-}
